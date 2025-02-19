b95d1a4918ba2f0f2b1599b899f9f53efc959556
NoobMaester <joshstilez@gmail.com>
2025-02-19 11:37:35 +0200

    changes to agents

--- src/pages/Agents.tsx
+++ src/pages/Agents.tsx
@@ -1,112 +1,72 @@
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-const Hero = () => {
+import { useEffect, useState } from "react";
+import { fetchUsers } from "./../services/api";
+
+interface User {
+  id: number;
+  name: {
+    first: string;
+    last: string;
+  };
+  email: string;
+  picture: {
+    large: string;
+  };
+}
+
+const UserList = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const data = await fetchUsers();
+        setUsers(data.results);
+        console.log(data.results);
+      } catch (error) {
+        setError(
+          `Failed to fetch users: ${
+            error instanceof Error ? error.message : "Unknown error"
+          }`
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getUsers();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
-    <div className="mt-16 p-4 flex justify-between flex-wrap
-    ">
-      <Card className="m-4" sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          height=""
-          image="/profile1.png"
-          alt="profile image"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            John Doe
-          </Typography>
-
-          <Typography variant="body1" color="text.secondary">
-            Umusare is a community-focused, online platform that connects
-            individuals with their local community.
-          </Typography>
-
-          <div className="mt-4">
-            <Button variant="contained" color="secondary">
-              Learn More...
-            </Button>
-          </div>
-        </CardContent>
-      </Card>
-
-      <Card className="m-4" sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          height=""
-          image="/profile1.png"
-          alt="profile image"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            John Doe
-          </Typography>
-
-          <Typography variant="body1" color="text.secondary">
-            Umusare is a community-focused, online platform that connects
-            individuals with their local community.
-          </Typography>
-
-          <div className="mt-4">
-            <Button variant="contained" color="secondary">
-              Learn More...
-            </Button>
-          </div>
-        </CardContent>
-      </Card>
-
-      <Card className="m-4" sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          height=""
-          image="/profile1.png"
-          alt="profile image"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            John Doe
-          </Typography>
-
-          <Typography variant="body1" color="text.secondary">
-            Umusare is a community-focused, online platform that connects
-            individuals with their local community.
-          </Typography>
-
-          <div className="mt-4">
-            <Button variant="contained" color="secondary">
-              Learn More...
-            </Button>
-          </div>
-        </CardContent>
-      </Card>
-
-      <Card className="m-4" sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          height=""
-          image="/profile1.png"
-          alt="profile image"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            John Doe
-          </Typography>
-
-          <Typography variant="body1" color="text.secondary">
-            Umusare is a community-focused, online platform that connects
-            individuals with their local community.
-          </Typography>
-          <div className="mt-4">
-            <Button variant="contained" color="secondary">
-              Learn More...
-            </Button>
-          </div>
-        </CardContent>
-      </Card>
+    <div className="p-6 max-w-4xl mt-16">
+      <h2 className="text-2xl font-bold mb-4 text-gray-800 text-center">
+        User List
+      </h2>
+      <ul className="flex justify-center items-center flex-wrap gap-4">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="grid items-center p-4 bg-gray-100 rounded-lg"
+          >
+            <img src={user.picture.large} alt="profile" />
+            <p className="text-gray-900 font-bold py-4">
+              {user.name.first} {user.name.last}
+            </p>
+            <p className="text-gray-600 py-2">{user.email}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default Hero;
+export default UserList;
---
+++ src/services/api.ts
@@ -0,0 +1,9 @@
+const API_URL = 'https://randomuser.me/api/';
+
+export const fetchUsers = async () => {
+  const response = await fetch(`${API_URL}/?results=10`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch users');
+  }
+  return response.json();
+};
